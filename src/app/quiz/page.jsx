23b99ff3bd59da4eb28fe5d39e08471ce9c19bbf
Hardik Garg <hardikg2907@gmail.com>
2024-02-28 23:37:36 +0530

    added interval again - testing over

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -82,9 +82,9 @@ const Page = () => {
           )
         );
       } else fetchData();
-      // window.interval = setInterval(() => {
-      //   setTimeRemaining((prev) => prev - 1);
-      // }, 1000);
+      window.interval = setInterval(() => {
+        setTimeRemaining((prev) => prev - 1);
+      }, 1000);
       return () => window.clearInterval(window.interval);
     }
   }, [quizStarted]);
